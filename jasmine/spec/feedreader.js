b55f93cf7135e570c77a2e0fefd0ff7f9841646c
Chikashi Miyama <chikashimiyama@gmail.com>
2018-01-24 14:36:59 +0100

    doc: more detailed comments

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -33,13 +33,16 @@ $(function() {
     describe('The menu', () => {
         const body = document.querySelector('body');
 
-        // we are hidding the menu by the css
-        // transform: translate3d(-12em, 0, 0);
-        // this is included in the class .menu-hidden and the class is applied to the body tag
+        /* we are hidding the menu by the css
+        transform: translate3d(-12em, 0, 0);
+        this is included in the class .menu-hidden and the class is applied to the body tag*/
+
+        /* this ensures the body tag has the class */
         it('is hidden by default', () => {
             expect(body.classList.contains('menu-hidden')).toBe(true);
         });
 
+        /* this simulates the click on menuIcon and checks the presence of menu-hidden tag after clicks */
         it('changes the visibility when the icon is clicked', () => {
             const menuIcon = document.querySelector('.menu-icon-link');
             const body = document.querySelector('body');
@@ -55,6 +58,7 @@ $(function() {
 
     describe('Initial Entries', ()=>{
 
+        /* done function will be executed after the feeds are loaded */
         beforeEach((done) => {
             loadFeed(0, done);
         });
@@ -63,6 +67,8 @@ $(function() {
             const container = document.querySelector('.feed');
             const elements = container.children;
             expect(elements.length).not.toBe(0);
+
+            /* ensures that there is an element with .entry class */
             for(const element of elements){
                 expect(element.querySelector('.entry')).toBeDefined();
             }
@@ -71,6 +77,9 @@ $(function() {
 
     });
 
+    /* this test load two different RSS feeds from different websites and ensures 
+    the actual change of the content by comparing the header title */
+    
     describe('New Feed Selection', ()=>{
         var preivousTitle;
 
